d8ddbbb1aa218bf0fe68a45fc9ed99240ddecf98
Ben Hathaway <b@hath.me>
2025-06-13 08:44:36 -0400

    Disable macos config script

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 source <(curl -s https://raw.githubusercontent.com/hathaway/dotfiles/HEAD/lib/echos.sh)
 source <(curl -s https://raw.githubusercontent.com/hathaway/dotfiles/HEAD/lib/requirers.sh)
 source <(curl -s https://raw.githubusercontent.com/hathaway/dotfiles/HEAD/bin/homebrew)
-source <(curl -s https://raw.githubusercontent.com/hathaway/dotfiles/HEAD/bin/macos)
+# source <(curl -s https://raw.githubusercontent.com/hathaway/dotfiles/HEAD/bin/macos)
